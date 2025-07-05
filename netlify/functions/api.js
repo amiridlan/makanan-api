b18baf0eeff4406750adf2af910fc9ed0ed5f605
amiridlan <53267532+amiridlan@users.noreply.github.com>
2025-07-05 18:03:46 +0800

    added cors

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,7 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const serverless = require('serverless-http');
 
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.send('API is running');
